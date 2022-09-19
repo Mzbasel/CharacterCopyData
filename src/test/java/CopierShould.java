4f7f16dd6c2016115121223f586708a79bd801d5
CrisMouta <crismouta@gmail.com>
2022-09-19 16:40:43 +0200

    Green: using mockito to verify how many times functions run

--- src/test/java/CopierShould.java
+++ src/test/java/CopierShould.java
@@ -1,11 +1,24 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 
+import static org.mockito.Mockito.*;
+
 class CopierShould {
+
+    @BeforeEach
+    void setUp() {
+        //if we don't call below, we will get NullPointerException
+        MockitoAnnotations.openMocks(this);
+    }
+    @Mock IDestination destination;
+    @Mock ISource source;
 
     @Test
     public void create_a_copy(){
@@ -31,6 +44,22 @@ class CopierShould {
         copier.Copy();
 
         Assertions.assertEquals(expected, destination.getResult());
+    }
+
+    @Test
+    public void verify_copy_run_once() {
+        when(source.GetChar()).thenReturn(
+                'a','b', 'c','\n'
+        );
+
+        doNothing().when(destination).SetChar(isA(Character.class));
+
+        Copier copier = new Copier(destination, source);
+        copier.Copy();
+
+
+        verify(source, times(4)).GetChar();
+        verify(destination, times(1)).SetChar('a');
     }
 
 
